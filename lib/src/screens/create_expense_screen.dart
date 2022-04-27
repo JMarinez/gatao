2b6bb8a8c125616a0e89bc8eed6bf7ab5e76bc57
Juan Marinez <juan.dsmarinez@gmail.com>
2022-04-26 23:55:18 -0400

    [ADD] wip create expense screen

--- lib/src/screens/create_expense_screen.dart
+++ lib/src/screens/create_expense_screen.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
 
-class CreateExpenseScreen extends StatelessWidget {
+class CreateExpenseScreen extends StatefulWidget {
   const CreateExpenseScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CreateExpenseScreen> createState() => _CreateExpenseScreenState();
+}
+
+class _CreateExpenseScreenState extends State<CreateExpenseScreen> {
+  final labelController = TextEditingController();
+  final amountController = TextEditingController();
+
+  String _label = '';
+  String _amount = '';
+
+  @override
+  void initState() {
+    labelController.addListener(() {
+      _label = labelController.text;
+    });
+
+    amountController.addListener(() {
+      _amount = amountController.text;
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,21 +39,40 @@ class CreateExpenseScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Text('data'),
-          Text('data'),
-          Text('data'),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+        child: Column(
+          children: [
+            _buildLabelField(),
+            const SizedBox(height: 10.0),
+            _buildAmountField(),
+          ],
+        ),
       ),
     );
   }
 
   Widget _buildLabelField() {
-    return TextField();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Expense label'),
+        TextField(
+          controller: labelController,
+        ),
+      ],
+    );
   }
 
   Widget _buildAmountField() {
-    return TextField();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Amount spent'),
+        TextField(
+          controller: amountController,
+        ),
+      ],
+    );
   }
 }
