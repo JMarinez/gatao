c7d4385f79f514335765f88f1b78db5a4eddae4a
Juan Marinez <juan.dsmarinez@gmail.com>
2022-05-05 23:54:49 -0400

    [ADD] Total money spent in dashboard with provider

--- lib/src/screens/dashboard_screen.dart
+++ lib/src/screens/dashboard_screen.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/expense_manager.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Hello Gatao',
-        style: TextStyle(fontSize: 28.0),
-      ),
-    );
+    return Consumer<ExpenseManager>(builder: (context, manager, child) {
+      return Center(
+        child: Text(
+          'Total spent: ${manager.totalSpent.toString()}',
+          style: const TextStyle(fontSize: 30.0),
+        ),
+      );
+    });
   }
 }
