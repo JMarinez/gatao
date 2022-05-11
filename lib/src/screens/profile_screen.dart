cdf6d58476c234f098f874dcebfcbc35dbfa7846
Juan Marinez <juan.dsmarinez@gmail.com>
2022-05-10 23:55:56 -0400

    [ADD] Profile page WIP

--- lib/src/screens/profile_screen.dart
+++ lib/src/screens/profile_screen.dart
@@ -5,10 +5,51 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Profile',
-        style: TextStyle(fontSize: 28.0),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(100)),
+                    child: const Icon(
+                      Icons.person,
+                      size: 90.0,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 15.0,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Username',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 10.0),
+                      Text(
+                        'Juan Mariñez',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const Icon(
+                Icons.edit,
+                size: 30.0,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
