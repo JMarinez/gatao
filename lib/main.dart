25136ea9bddf8a509267246adb24d3bab77b7935
Juan Marinez <juan.dsmarinez@gmail.com>
2022-05-12 23:56:39 -0400

    [ADD] Google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gatao/src/models/expense_manager.dart';
 import 'package:gatao/src/models/tab_manager.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'home.dart';
 
@@ -18,8 +19,18 @@ class GataoApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
+          primarySwatch: Colors.red,
+          primaryTextTheme: TextTheme(
+            headline1:
+                GoogleFonts.inter(fontSize: 32.0, fontWeight: FontWeight.bold),
+            headline2:
+                GoogleFonts.inter(fontSize: 24.0, fontWeight: FontWeight.bold),
+            headline3:
+                GoogleFonts.inter(fontSize: 18.0, fontWeight: FontWeight.bold),
+            bodyText1: GoogleFonts.inter(fontSize: 16.0),
+            bodyText2:
+                GoogleFonts.inter(fontSize: 16.0, fontWeight: FontWeight.bold),
+          )),
       home: MultiProvider(
         providers: [
           ChangeNotifierProvider(create: (context) => TabManager()),
