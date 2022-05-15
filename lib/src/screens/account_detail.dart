15896ebcb78b32717469ca8adeb7e95b856e7168
Juan Marinez <juan.dsmarinez@gmail.com>
2022-05-14 23:51:27 -0400

    [ADD] Account detail screen

---
+++ lib/src/screens/account_detail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AccountDetail extends StatelessWidget {
+  const AccountDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
