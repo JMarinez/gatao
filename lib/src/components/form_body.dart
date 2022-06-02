41729b152e32d748855ea25f19343a6316d386f6
Juan Marinez <juan.dsmarinez@gmail.com>
2022-06-01 23:51:16 -0400

    [ADD] Reusable form body component WIP

---
+++ lib/src/components/form_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FormBody extends StatelessWidget {
+  const FormBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
